75e0547d4b5dbb77c658c79a1c097cb2c4b13904
Aaditya Sutar <sutaraaditya47@gmail.com>
2025-01-07 13:20:20 +0530

    Add files via upload

---
+++ DotNet/Practise/EmployeeAdminPortal/EmployeeAdminPortal/Data/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ DotNet/Practise/EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Identity.Client;
+
+namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; } 
+        public required string Name { get; set; } 
+        public  required string Email { get; set; }
+
+        public string? Phone { get; set; } 
+
+        public decimal Salary { get; set; }
+
+    }
+}
